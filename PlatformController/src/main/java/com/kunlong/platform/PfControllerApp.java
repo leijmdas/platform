fc9047e3e708288d94d5159b37d0f9fd14275c59
leijmdas <leijmdas@163.com>
2019-12-14 10:03:34 +0800

    add

--- PlatformController/src/main/java/com/kunlong/platform/PfControllerApp.java
+++ PlatformController/src/main/java/com/kunlong/platform/PfControllerApp.java
@@ -4,6 +4,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// mvn deploy:deploy-file -DgroupId=app -DartifactId=app-support -Dversion=1.1 -Dpackaging=jar -Dfile=app-support-1.1.jar -Durl=http://localhost:8081/repository/maven-releases/ -DrepositoryId=nexus
 
 @SpringBootApplication(scanBasePackages = {"com.kunlong"})
 
